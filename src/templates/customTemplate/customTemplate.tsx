08ebf0da4b0fb70511532bc5ee89d85aebfb59ff
samarth30 <samarth.gugnani30@gmail.com>
2020-06-22 04:45:45 +0530

    added

--- src/templates/customTemplate/customTemplate.tsx
+++ src/templates/customTemplate/customTemplate.tsx
@@ -3,28 +3,29 @@ import { TemplateProps } from "@govtechsg/decentralized-renderer-react-component
 import { css } from "@emotion/core";
 import { CustomTemplateCertificate } from "../sample";
 import { PrintWatermark } from "../../core/PrintWatermark";
-import logo from "./a.jpg";
+import logo from "./aaa.jpeg";
 
 const containerStyle = css`
   background-: #324353;
   color: #ffffff;
   padding: 15px;
   margin: auto;
-  width: 80%;
+  width: 100%;
+  height: 100%;
   text-align: center;
 `;
-
+// css={containerStyle}
 export const CustomTemplate: FunctionComponent<TemplateProps<CustomTemplateCertificate> & { className?: string }> = ({
   document,
   className = ""
 }) => {
   return (
-    <div css={containerStyle} className={className} id="custom-template">
-      <PrintWatermark />
-      <h1>{document.name}</h1>
+    <div className="Container mr-100">
+      {/* <PrintWatermark />
+      <h1>{document.name}</h1> */}
       <img src={logo} />
-      <div>awarded to</div>
-      <h2>{document.recipient.name}</h2>
+      {/* <div>awarded to</div>
+      <h2>{document.recipient.name}</h2> */}
     </div>
   );
 };
